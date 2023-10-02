063e57776d081e8611ad60adc0fd0afcfd714f09
Yamac <146765030+yamackl@users.noreply.github.com>
2023-10-02 21:42:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL": "Bir şakaya karşılık cevap",
+            "SHEESH": "Onaylamamak",
+            "CREEPY": "Korkunç",
+            "AGGRO": "Agresifleşmek"
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
 
+if word in meme_dict.keys():
+    print(meme_dict)
+else:
+    print("Bu kelime sözlükte bulunmamaktadır")
